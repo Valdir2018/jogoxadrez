59498f8fed591192fc488b5c7cbb4bb7e16d0b93
Valdir Silva <valdirpiresba@gmail.com>
2019-09-26 23:43:38 -0300

    Criando a classe Posicao

--- projeto_jogo_xadrez/Program.cs
+++ projeto_jogo_xadrez/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace projeto_jogo_xadrez
 {
@@ -10,6 +11,15 @@ namespace projeto_jogo_xadrez
     {
         static void Main(string[] args)
         {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+
+            Console.WriteLine("POSIÇÃO: " + p);
+            Console.ReadLine();
+
+
         }
     }
 }
